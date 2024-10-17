489c9d91878c0f6361335df5bf19d62f6402fd3e
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2024-10-18 00:21:24 +0200

    Use French keycodes

--- keymap.c
+++ keymap.c
@@ -2,26 +2,24 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_french.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
-      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
-      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
-      * ├───┼───┼───┼───┼───┘       └───┼───┼───┼───┼───┤
-      * │ Z │ X │ C │ V │               │ M │ , │ . │ / │
-      * └───┴───┴───┴───┘               └───┴───┴───┴───┘
-      *           ┌───┐                   ┌───┐
-      *           │ B ├───┐           ┌───┤ N │
-      *           └───┤Bsp├───┐   ┌───┤Ent├───┘
-      *               └───┤Alt│   │   ├───┘
-      *                   └───┘   └───┘
-      */
-    [0] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,                                                 KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                     KC_B, KC_BSPC, KC_RALT,           KC_SPC,  KC_ENT,  KC_N
-    )
-};
+    /*
+     * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+     * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+     * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+     * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+     * ├───┼───┼───┼───┼───┘       └───┼───┼───┼───┼───┤
+     * │ Z │ X │ C │ V │               │ M │ , │ . │ / │
+     * └───┴───┴───┴───┘               └───┴───┴───┴───┘
+     *           ┌───┐                   ┌───┐
+     *           │ B ├───┐           ┌───┤ N │
+     *           └───┤Bsp├───┐   ┌───┤Ent├───┘
+     *               └───┤Alt│   │   ├───┘
+     *                   └───┘   └───┘
+     */
+    [0] = LAYOUT(FR_Q, FR_W, FR_E, FR_R, FR_T, FR_Y, FR_U, FR_I, FR_O, FR_P,
+                 FR_A, FR_S, FR_D, FR_F, FR_G, FR_H, FR_J, FR_K, FR_L, FR_SCLN,
+                 FR_Z, FR_X, FR_C, FR_V, FR_M, FR_COMM, FR_DOT, FR_SLSH, FR_B,
+                 KC_BSPC, KC_RALT, KC_SPC, KC_ENT, FR_N)};
